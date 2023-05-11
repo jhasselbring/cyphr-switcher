d6b4d5d95da2f80e5d785fed6286c1ba3f1fa006
JHasselbring <janmykhail.hasselbring@outlook.com>
2023-05-10 21:24:36 -0500

    Update

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const lockTransform = (offset) => {
 const unlockTransform = (offset) => {
     return switcher(offset * -1);
 }
-
+const lock = (string, offset) => {
+    return flipper(string, offset);
+}
+const unlock = (string, offset) => {
+    return flipper(string, offset * -1);
+}
 const switcher = (offset) => {
     return new Transform({
         transform(chunk, encoding, callback) {
@@ -23,7 +28,24 @@ const switcher = (offset) => {
         },
     });
 }
+const flipper = (string, offset) => {
 
+    let pendingArray = [];
+    let buff = Buffer.from(string);
+    for (const value of buff.values()) {
+        pendingArray.push(value + offset);
+        if (typeof value != 'number') {
+            console.log('Not a number');
+        }
+    }
+    let chunkBuffer = Buffer.from(pendingArray);
+    return chunkBuffer.toString();
+
+};
 exports.lockTransform = lockTransform;
 exports.unlockTransform = unlockTransform;
 exports.switcher = switcher;
+
+exports.lock = lock;
+exports.unlock = unlock;
+exports.flipper = flipper;
